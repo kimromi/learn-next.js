60e22949ca1160d3405fa19c37ac3eca091b18d2
kimromi <kimromi4@gmail.com>
2019-02-28 10:54:46 +0900

    クラス形式で書いてみる

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,9 @@
 import React from 'react'
-export default () => <div>Hello world!</div>
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
