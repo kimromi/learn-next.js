9c66fe006122a67dffaa9b1c994b0c0e854db46e
kimromi <kimromi4@gmail.com>
2019-02-28 11:03:59 +0900

    コンストラクタで変数をもたせてみる

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default class extends React.Component {
+  constructor() {
+    super()
+    this.where = 'world'
+  }
+
   render() {
     return (
-      <div>Hello world!</div>
+      <div>hello { this.where }!</div>
     )
   }
 }
