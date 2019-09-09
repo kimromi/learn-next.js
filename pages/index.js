e94697b3ba9b8b872702375c206c5f1786a1eb4a
kimromi <kimromi4@gmail.com>
2019-09-09 13:48:12 +0900

    head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Head from 'next/head'
+
 import "../assets/stylesheets/index.scss"
 
 export default class extends React.Component {
@@ -10,6 +12,11 @@ export default class extends React.Component {
   render() {
     return (
       <div>
+        <Head>
+          <title>Index page</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+
         <h1>hello { this.where }!</h1>
         <img src="static/mondorian.jpg" />
       </div>
