985697c4ebecaa977847805fa5221e441fbc21c5
kimromi <kimromi4@gmail.com>
2021-01-07 00:54:31 +0900

    add external links

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import Link from 'next/link';
 
-export default function Home({ blog }) {
+const externalLinks = [
+  {publishedAt: '2021-01-06 23:18:30', title: 'external1', href: 'https://github.com/kimromi'}
+];
+
+export default function Home({posts}) {
   return (
     <div>
       <ul>
-        {blog.map(blog => (
-          <li key={blog.id}>
-            <Link href={`blog/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
+        {posts.map(post => (
+          post.href ?
+            <li key={post.href}>
+              <a href={post.href} target="_blank">
+                {post.title}
+              </a>
+            </li>
+            :
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
         ))}
       </ul>
     </div>
@@ -25,9 +36,21 @@ export const getStaticProps = async () => {
   const data = await fetch('https://kimromi.microcms.io/api/v1/blog', key)
     .then(res => res.json())
     .catch(() => null);
+
+  const posts = data.contents.concat(externalLinks)
+  posts.sort((a, b) => {
+    if (Date.parse(a.publishedAt) < Date.parse(b.publishedAt)) {
+      return 1;
+    }
+    if (Date.parse(a.publishedAt) > Date.parse(b.publishedAt)) {
+      return -1;
+    }
+    return 0;
+  });
+
   return {
     props: {
-      blog: data.contents,
+      posts,
     },
   };
 };
